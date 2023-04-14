f8addab3ac4b722a699bba28d8f8712facb50158
kajal1801 <anshikajais2001@gmail.com>
2023-04-14 14:53:54 +0530

    Updated js location

---
+++ js/display.js
@@ -0,0 +1,32 @@
+const displayForm = document.querySelector('#displayForm')
+const previewRollnum = document.querySelector('#previewRollnum')
+const display = document.querySelector('#display')
+
+
+displayForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    preview()
+})
+
+
+function preview(){
+    const rollnum = previewRollnum.value.trim()
+    const data = localStorage.getItem(rollnum)
+    console.log(JSON.parse(data))
+
+    if(data === null){
+        display.innerHTML = `
+        <h1>Student Not Found</h1>
+        `
+    } else {
+        display.innerHTML = `
+        <div class="data">
+            <h1>Student Details</h1>
+            <p>Name: ${JSON.parse(data).name}</p>
+            <p>Roll Number: ${JSON.parse(data).rollnum}</p>
+            <p>Course: ${JSON.parse(data).course}</p>
+            <p>Department: ${JSON.parse(data).dept}</p>
+        </div>
+        `
+    }
+}
